55c63cb4489bb3dcf5b3e6a39a135ce74bca36bf
BhuOne Joshi <156088164+bhuvanjaishi@users.noreply.github.com>
2026-01-07 07:14:30 +0545

    Refactor navbar and typing effects in script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 
 
     // Index pahe BhuOne Joshi Name script
-  const name = "B.J";  
+  const name = "BhuOne Joshi";  
   const span = document.getElementById("highlight");
   let index = 0;
 
@@ -124,6 +124,7 @@
       });
     });
   });
+
 
 
 
